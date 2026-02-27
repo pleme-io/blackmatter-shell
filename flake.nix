25f78814eeea4b0d9dc406b5c5e33d5c75a2cb87
drzzln <drzzln@protonmail.com>
2026-02-27 09:20:53 -0500

    chore: switch nixpkgs to nixos-25.11 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Blackmatter Shell - curated zsh distribution with 7 plugins and 35 bundled tools";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/d6c71932130818840fc8fe9509cf50be8c64634f";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     blackmatter-nvim = {
       url = "github:pleme-io/blackmatter-nvim";
       inputs.nixpkgs.follows = "nixpkgs";
